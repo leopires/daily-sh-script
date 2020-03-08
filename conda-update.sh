f8d8568fd5182151581eff31df16238c710685c0
Leonardo Pires <lbpires@gmail.com>
2020-03-08 09:23:25 -0300

    - Conda update script.

---
+++ conda-update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+CONDA_PATH=`which conda`
+
+if [ -z "${CONDA_PATH}" ]; then
+    echo "Please check if you have 'conda' installed."
+    exit 1;
+else
+    echo "'conda' found at: '${CONDA_PATH}'"
+fi
+
+conda update --all -y
+
+conda clean --all -y
+
+exit 0
